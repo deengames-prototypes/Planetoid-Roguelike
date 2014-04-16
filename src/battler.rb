c6e94e761dd2918c75eefd88f135947e21ba1a39
Ashiq A <ashes999@yahoo.com>
2014-04-16 16:04:18 -0400

    Simplified battling/checking

--- src/battler.rb
+++ src/battler.rb
@@ -11,16 +11,14 @@ class Battler
 		attacks.each do |a|
 			attacker = a[:attacker]
 			target = a[:target]
-			
-			if attacker.has?(:battle) then
-				damage = attacker.get(:battle).strength
-				health = target.get(:health)
-				health.get_hurt(damage)
-										
-				message = "#{attacker.name} attacks #{target.name} for #{damage} damage!"
-				message += " #{target.name} dies!" if !health.is_alive?
-				messages << message
-			end
+						
+			damage = attacker.get(:battle).strength
+			health = target.get(:health)			
+			health.get_hurt(damage)
+									
+			message = "#{attacker.name} attacks #{target.name} for #{damage} damage!"
+			message += " #{target.name} dies!" if !health.is_alive?
+			messages << message
 		end
 		return {:messages => messages}
 	end
