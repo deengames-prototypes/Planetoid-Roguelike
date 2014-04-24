5e7c2a2f8e142bd049f2dad54e17cdbb4fda1342
Ashiq A <ashes999@yahoo.com>
2014-04-24 17:07:12 -0400

    Fix bug with dead enemies attacking

--- src/battler.rb
+++ src/battler.rb
@@ -10,14 +10,22 @@ class Battler
 		attacks.each do |a|
 			attacker = a[:attacker]
 			target = a[:target]
-						
-			damage = attacker.get(:battle).strength
-			health = target.get(:health)			
-			health.get_hurt(damage) if damage > 0
+			# Attacks are already ordered. By the time your turn comes, you may
+			# already be dead. Check and verify.
+			if (attacker.get(:health).is_alive?) then
+				damage = attacker.get(:battle).strength
+				health = target.get(:health)			
+				health.get_hurt(damage) if damage > 0
 
-			message = "#{attacker.name} attacks #{target.name} for #{damage} damage!"
-			message += " #{target.name} dies!" if !health.is_alive?
-			messages << message
+				message = "#{attacker.name} attacks #{target.name} for #{damage} damage!"
+				if !health.is_alive?
+					message += " #{target.name} dies!" 
+					if attacker.name.downcase == 'player' then
+						attacker.get(:experience).gain_experience(target.experience)
+					end
+				end
+				messages << message
+			end
 		end
 		return {:messages => messages}
 	end
--- src/monster.rb
+++ src/monster.rb
@@ -13,13 +13,17 @@ class Monster < Hatchling::Entity
 			health = 15 + rand(5)
 			strength = 2 + rand(1)
 			speed = 1
+			experience = 10
 		else
 			raise "Not sure how to make a monster of type #{type}"
 		end
 		
+		raise "Invalid monster: hp=#{health}, str=#{strength}, speed=#{speed} xp=#{experience}" if health.nil? || strength.nil? || speed.nil? || experience.nil?
+		
 		components[:health] = HealthComponent.new(health)
 		components[:battle] = BattleComponent.new({:strength => strength, :speed => speed, :target => target})
 		components[:name] = "Drone"
+		components[:experience] = experience
 		
 		super(components)
 	end
