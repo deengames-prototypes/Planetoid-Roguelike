5799e7e72939b5b0b2dc77cae6b84a3c78c13839
ashes999 <ashes999@yahoo.com>
2014-05-02 14:57:54 -0400

    Add missing (not checked in) test file.

---
+++ test/src/monster_test.rb
@@ -0,0 +1,32 @@
+require_relative '../test_config'
+require_relative "#{SOURCE_ROOT}/src/monster"
+
+class MonsterTest < Test::Unit::TestCase
+	
+	def test_initialize_gives_drones_required_components_and_data
+		m = Monster.new(10, 13, :drone, nil)
+		
+		# Check for components
+		[:display, :battle, :health].each do |c|
+			assert(m.has?(c))
+		end		
+		
+		d = m.get(:display)
+		assert_not_nil(d)
+		assert_equal(10, d.x)
+		assert_equal(13, d.y)
+		assert_equal('d', d.character)
+		
+		b = m.get(:battle)
+		assert_not_nil(b)
+		assert(b.strength > 1)
+		assert(b.speed >= 1)
+		
+		h = m.get(:health)
+		assert_not_nil(h)
+		assert(h.max_health > 0)
+		
+		assert_not_nil(m.get(:name))
+		assert_not_nil(m.get(:experience))
+	end
+end
