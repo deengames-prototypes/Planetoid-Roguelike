6faf7b4b9f191fad311ff64e6b648f312e6dcb51
ashes999 <ashes999@yahoo.com>
2014-06-12 15:43:28 -0400

    Tweaked level XP algo

--- main.rb
+++ main.rb
@@ -8,7 +8,9 @@ require_relative 'src/battler'
 player = Entity.new({
 	:health => HealthComponent.new(50, 0.02),
 	:battle => BattleComponent.new({:strength => 7, :speed => 3 }),	
-	:experience => ExperienceComponent.new(lambda { |level| return (level**2 * 50) + (level * 100) })
+	# Total XP: 150, 400, 750, 1200, ...
+	# XP diff: 150, 250, 350, 450, ...
+	:experience => ExperienceComponent.new(lambda { |level| return (50*(level - 1)**2) + ((level - 1) * 100) })
 })
 
 g = Hatchling::Game.new(:player => player)
